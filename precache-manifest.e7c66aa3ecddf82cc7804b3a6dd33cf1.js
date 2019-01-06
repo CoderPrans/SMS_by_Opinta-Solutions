bf5f56b675e6210ce66ee7a8f367a5f6382a8b95
CoderPrans <pranavbhaskar17@gmail.com>
2019-01-06 23:56:27 +0530

    Updates

---
+++ precache-manifest.e7c66aa3ecddf82cc7804b3a6dd33cf1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a219cd2f7059fb95118",
+    "url": "/SMS_by_Opinta-Solutions/static/js/runtime~main.4a219cd2.js"
+  },
+  {
+    "revision": "097196a4d03ed26f3f30",
+    "url": "/SMS_by_Opinta-Solutions/static/js/main.097196a4.chunk.js"
+  },
+  {
+    "revision": "f715b2e0bea123466b14",
+    "url": "/SMS_by_Opinta-Solutions/static/js/1.f715b2e0.chunk.js"
+  },
+  {
+    "revision": "097196a4d03ed26f3f30",
+    "url": "/SMS_by_Opinta-Solutions/static/css/main.9d28f5d8.chunk.css"
+  },
+  {
+    "revision": "dc0d16bad42a960fbe39944bba002c8e",
+    "url": "/SMS_by_Opinta-Solutions/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/SMS_by_Opinta-Solutions/precache-manifest.e7c66aa3ecddf82cc7804b3a6dd33cf1.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/SMS_by_Opinta-Solutions/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
